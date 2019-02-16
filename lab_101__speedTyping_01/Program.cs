3f03eff784c5b10435555482a1de6a284ea44005
Sam Bowden-Williams <SBowdenWilliams@spartaglobal.com>
2019-02-16 13:48:24 +0000

    Added speed typing game

--- lab_101__speedTyping_01/Program.cs
+++ lab_101__speedTyping_01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace lab_101__speedTyping_01
 {
@@ -10,6 +11,35 @@ namespace lab_101__speedTyping_01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter characters before time is up\n");
+            Console.WriteLine("Enter S to start\n");
+
+            char userInput = Console.ReadKey().KeyChar;
+
+            if (userInput == 's' || userInput == 'S')
+            {
+                Console.WriteLine("Enter how many seconds you wish to play for");
+                int timeInput = Convert.ToInt32(Console.ReadLine());
+                game(timeInput);
+            }
+        }
+
+
+
+        public static void game(int time)
+        {
+            List<char> inputList = new List<char>();
+            char input;
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            while (timer.Elapsed.TotalSeconds < time)
+            {
+                input = Console.ReadKey().KeyChar;
+                inputList.Add(input);
+            }
+            Console.WriteLine("\n\n Times up\n\n");
+            Console.WriteLine($"You entered {inputList.Count().ToString()} in the elapsed time!");
         }
     }
 }
